78c1dada42cea99b764bf2b6d411176623490a16
xuxu <thaynaracdutra@outlook.com>
2022-07-06 21:24:07 -0300

    Tesde de qualidade Itaú

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -2,9 +2,7 @@ describe('Automação de cenário de teste número 2', () => {
   beforeEach(() => {
     cy.visit("https://www.itau.com.br/")})
 
-
     it('Acesso as informações de como criar conta corrente', () => {
-
       cy.location("pathname").should("eq", "/")
 
       cy.get(':nth-child(1) > :nth-child(1) > :nth-child(1) > .dropdown-toggle').click()
